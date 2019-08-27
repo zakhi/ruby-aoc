aff3bbce0904276355f384e13a68ac4db44e37df
Zakhi Verech <zakhi@omnisol.co.il>
2019-08-27 10:46:31 +0300

    Day 7 Part 2

--- day_7.rb
+++ day_7.rb
@@ -1,18 +1,83 @@
-precedences = Hash.new { |hash, key| hash[key] = [] }
+class Step
+  attr_reader :letter, :depends_on, :time_left
 
-File.readlines("input/day_7").map do |line|
-  first, second = /Step (\w) must be finished before step (\w) can begin/.match(line).to_a.drop(1)
-  precedences[second] << first
+  def initialize(letter)
+    @letter = letter
+    @time_left = letter.ord - "A".ord + 61
+    @depends_on = []
+  end
+
+  def can_start?
+    !completed? and @depends_on.all?(&:completed?)
+  end
+
+  def completed?
+    @time_left == 0
+  end
+  
+  def work(seconds=@time_left)
+    @time_left -= seconds
+  end
 end
 
-steps_left = precedences.values.flat_map(&:itself).uniq.sort
+
+Rules = File.readlines("input/day_7").map do |line|
+  /Step (\w) must be finished before step (\w) can begin/.match(line).to_a.drop(1)
+end
+
+def create_steps
+  steps = Rules.flat_map(&:itself).uniq.map { |letter| [letter, Step.new(letter)] }.to_h
+  Rules.each do |dependency, dependent|
+    steps[dependent].depends_on << steps[dependency]
+  end
+  steps.values
+end
+
+steps = create_steps
 steps_taken = []
 
-while !steps_left.empty?
-  next_step = steps_left.find do |step| 
-    precedences[step].all? { |preceding| steps_taken.include? preceding }
-  end
-  steps_taken << steps_left.delete(next_step)
+until steps.all?(&:completed?)
+  next_step = steps.find(&:can_start?)
+  steps_taken << next_step.letter
+  next_step.work
 end
 
-puts "#{steps_taken.join}"
+puts "Part 1: #{steps_taken.join}"
+
+class Worker
+  def assign(step)
+    @step = step
+  end
+
+  def idle?
+    @step.nil?
+  end
+
+  def work
+    if @step
+      @step.work(1)
+      @step = nil if @step.completed?
+    end
+  end
+end
+
+steps = create_steps
+steps_left = steps.clone
+workers = 5.times.map { Worker.new }
+time = 0
+
+until steps.all?(&:completed?)
+  loop do
+    worker = workers.find(&:idle?)
+    step = steps_left.find(&:can_start?)
+
+    break if !worker or !step
+    worker.assign(step)
+    steps_left.delete(step)
+  end
+
+  workers.each(&:work)
+  time += 1
+end
+
+puts "Part 2: #{time}"
