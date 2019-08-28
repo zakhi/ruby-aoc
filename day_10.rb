50c044c8c9fd134b343db97fd67d62abc3ea894a
Zakhi Verech <zakhi@omnisol.co.il>
2019-08-28 09:21:00 +0300

    Day 10

---
+++ day_10.rb
@@ -0,0 +1,41 @@
+require_relative "helpers/enumerable"
+
+Point = Struct.new(:x, :y, :vx, :vy) do
+  def position_after(seconds)
+    [x + seconds * vx, y + seconds * vy]
+  end
+end
+
+points = File.readlines("input/day_10").map do |line|
+  match = /<\s?(-?\d+), \s?(-?\d+)> .*<\s?(-?\d+), \s?(-?\d+)>/.match(line)
+  Point.new(*match.values_at(*1..4).map(&:to_i))
+end
+
+def total_distance(points, time=0)
+  xs = points.map { |p| p.position_after(time).first }
+  xs.max - xs.min
+end
+
+def print(points, time)
+  positions = points.map { |p| p.position_after(time) }
+  xs = Range.new(*positions.map(&:first).minmax)
+  ys = Range.new(*positions.map(&:last).minmax)
+
+  for y in ys
+    puts xs.map { |x| positions.include?([x, y]) ? "#" : "." }.join
+  end
+  puts
+end
+
+previous_distance = total_distance(points)
+
+message_time = NaturalNumbers.each do |time|
+  distance = total_distance(points, time)
+  break time - 1 if distance > previous_distance
+  previous_distance = distance
+end
+
+puts "Part 1:"
+print(points, message_time)
+
+puts "Part 2: #{message_time}"
--- helpers/enumerable.rb
+++ helpers/enumerable.rb
@@ -3,3 +3,11 @@ module Enumerable
     group_by(&:itself).transform_values(&:count)
   end
 end
+
+NaturalNumbers = Enumerator.new do |y|
+  current = 1
+  loop do
+    y << current
+    current += 1
+  end
+end
