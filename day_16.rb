074854e007d421a82bbe635a64f970a9606d0f9a
Zakhi Verech <zakhi@omnisol.co.il>
2019-09-13 23:00:36 +0300

    Day 19

--- day_16.rb
+++ day_16.rb
@@ -1,23 +1,5 @@
 require "strscan"
-
-OPERATIONS = {
-  addr: -> (a, b, c, regs) { regs[c] = regs[a] + regs[b] },
-  addi: -> (a, b, c, regs) { regs[c] = regs[a] + b },
-  mulr: -> (a, b, c, regs) { regs[c] = regs[a] * regs[b] },
-  muli: -> (a, b, c, regs) { regs[c] = regs[a] * b },
-  banr: -> (a, b, c, regs) { regs[c] = regs[a] & regs[b] },
-  bani: -> (a, b, c, regs) { regs[c] = regs[a] & b },
-  borr: -> (a, b, c, regs) { regs[c] = regs[a] | regs[b] },
-  bori: -> (a, b, c, regs) { regs[c] = regs[a] | b },
-  setr: -> (a, b, c, regs) { regs[c] = regs[a] },
-  seti: -> (a, b, c, regs) { regs[c] = a },
-  gtir: -> (a, b, c, regs) { regs[c] = a > regs[b] ? 1 : 0 },
-  gtri: -> (a, b, c, regs) { regs[c] = regs[a] > b ? 1 : 0 },
-  gtrr: -> (a, b, c, regs) { regs[c] = regs[a] > regs[b] ? 1 : 0 },
-  eqir: -> (a, b, c, regs) { regs[c] = a == regs[b] ? 1 : 0 },
-  eqri: -> (a, b, c, regs) { regs[c] = regs[a] == b ? 1 : 0 },
-  eqrr: -> (a, b, c, regs) { regs[c] = regs[a] == regs[b] ? 1 : 0 }
-}
+require_relative "common/operations"
 
 class Sample
   attr_reader :code
---
+++ day_19.rb
@@ -0,0 +1,44 @@
+require_relative "common/operations"
+
+class Instruction
+  def initialize(name, a, b, c)
+    @name, @a, @b, @c = name, a, b, c
+  end
+
+  def run(registers)
+    OPERATIONS[@name][@a, @b, @c, registers]
+  end
+
+  def to_s
+    "#{@name} #{@a} #{@b} #{@c}"
+  end
+end
+
+lines = File.readlines("input/day_19")
+ip_register = lines.shift.match(/#ip (\d+)/)[1].to_i
+
+instructions = lines.map do |line|
+  name, a, b, c = line.match(/(\w+) (\d+) (\d+) (\d+)/)[1..4]
+  Instruction.new(name.to_sym, a.to_i, b.to_i, c.to_i)
+end
+
+def run_process(instructions, registers, ip_register)
+  ip = 0
+
+  while ip < instructions.size
+    registers[ip_register] = ip
+    instructions[ip].run(registers)
+    ip = registers[ip_register] + 1
+  end
+end
+
+registers = [0, 0, 0, 0, 0, 0]
+run_process(instructions, registers, ip_register)
+
+puts "Part 1: #{registers[0]}"
+
+def divisors_sum(num)
+  (1..num).lazy.select { |i| num % i == 0 }.sum
+end 
+
+puts "Part 2: #{divisors_sum(10551340)}"
